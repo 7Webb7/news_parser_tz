4ee646c616f05859130cea98d4e9882c7c20a3bf
7Webb7 <v.dovybortsev@gmail.com>
2024-08-11 06:51:44 +0500

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 
+def get_news():
+    url = entry_url.get()
+    text_bottom.config(text=f"Новость по ссылке {url}")
+
 root = tk.Tk()
 root.title("Petrovich News")
 root.geometry("350x350")
@@ -14,8 +18,11 @@ text.pack()
 entry_url = tk.Entry(root)
 entry_url.pack()
 
-button_get = tk.Button(root, text="Подтвердить")
+button_get = tk.Button(root, text="Подтвердить", command=get_news)
 button_get.pack()
+
+text_bottom = tk.Label(root, text="Необходимо ввести ссылку")
+text_bottom.pack()
 
 background_label = tk.Label(root, image=background_image)
 background_label.pack()

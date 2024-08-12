927310e9536f1e2390ba0fae5f13efb56a62b3d5
7Webb7 <v.dovybortsev@gmail.com>
2024-08-12 14:05:22 +0500

    first commit

--- main.py
+++ main.py
@@ -1,9 +1,48 @@
 import tkinter as tk
+import requests
+from tkinter import messagebox
 from PIL import Image, ImageTk
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+
+def is_valid_url(url):
+    parsed_url = urlparse(url)
+    return all([parsed_url.scheme, parsed_url.netloc])
 
 def get_news():
     url = entry_url.get()
-    text_bottom.config(text=f"Новость по ссылке {url}")
+    if not is_valid_url(url):
+        messagebox.showerror("Ошибка", "Введите корректный URL")
+        return
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        messagebox.showerror("Ошибка", f"Не удалось получить данные с URL: {e}")
+        return
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Извлекаем заголовок страницы
+    title = soup.title.string if soup.title else "Без заголовка"
+
+    # Извлекаем текст страницы
+    paragraphs = soup.find_all("p", class_="topic-body__content-text")
+    if paragraphs:
+        body_text = "\n\n".join(p.get_text() for p in paragraphs)
+    else:
+        body_text = "Текст новости не найден."
+
+    # Сохраняем заголовок и текст в файл
+    filename = "news.txt"
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(f"Заголовок: {title}\n\n")
+        file.write(body_text)
+
+    # Обновляем поле вывода
+    text_bottom.config(text="Файл сохранен")
+
 
 root = tk.Tk()
 root.title("Petrovich News")
